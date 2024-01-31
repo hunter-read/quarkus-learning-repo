827aaf6969db8fc5650fc2368335193fb673a224
lorenzo <lorenzo.appolinaire@cognosante.com>
2024-01-31 13:59:03 -0500

    quarkus test project changes

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,44 +1,68 @@
 package org.acme;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.acme.models.User;
 import org.acme.service.UserService;
-import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
-import org.eclipse.microprofile.rest.client.inject.RestClient;
-import org.proxies.TvSeriesProxy;
+import org.jboss.logging.Logger;
 
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+
+
 
 @Path("/users")
 public class GreetingResource {
 
     @Inject
     UserService userService;
-
+    @Inject
+    Logger logger;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response fetchTvSeriesDeails() {
-       
+    public Response fetchUserDetails() {
+       logger.info("returning all users");
         return userService.fetchUsers();
     }
+
+    @GET
+    @Path("/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response fetchUserDetailbyName(@PathParam("name") String userName) {
+       logger.info("returning users with name : " + userName);
+        return userService.fetchUsersByName(userName);
+    }
+
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response addNewUser(@RequestBody User user) {
+    public Response addNewUser(User user) {
+        logger.info("creating new users");
         return userService.createNewUser(user);
     }
 
-   
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response updateUser(User user) {
+        logger.info("updating  users");
+        return userService.updateUser(user);
+    }
+    @DELETE
+    @Path("/{name}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteUser(@PathParam("name") String userName) {
+        logger.info("deleting  users");
+        return userService.deleteUser(userName);
+    }
 
 }
